6de509304ac7fc15fdf544891b8fd228350cbc27
ksmithut <ksmith.ut@gmail.com>
2017-12-01 11:01:51 -0700

    day 1

--- days/day01.js
+++ days/day01.js
@@ -3,15 +3,22 @@
 // Part 1
 // ======
 
-const part1 = input => {
-  return input
+const sumMatchingPairs = (input, getNextIndex) => {
+  return input.split('').reduce((sum, char, i, arr) => {
+    const nextIndex = getNextIndex(i, arr)
+    return sum + (char === arr[nextIndex] ? +char : 0)
+  }, 0)
 }
+
+const getNeighborIndex = (i, arr) => (i >= arr.length - 1 ? 0 : i + 1)
+
+const part1 = input => sumMatchingPairs(input, getNeighborIndex)
 
 // Part 2
 // ======
 
-const part2 = input => {
-  return input
-}
+const getOppositeIndex = (i, arr) => (i + arr.length / 2) % arr.length
+
+const part2 = input => sumMatchingPairs(input, getOppositeIndex)
 
 module.exports = { part1, part2 }
