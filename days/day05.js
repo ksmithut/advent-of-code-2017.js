a8ba87fbfbce0f919cd0bd283a1bef700783d36c
ksmithut <ksmith.ut@gmail.com>
2017-12-05 05:36:24 -0700

    day5

---
+++ days/day05.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// Part 1
+// ======
+
+const followInstructions = (input, increment) => {
+  const instructions = input.split('\n').map(n => parseInt(n, 10))
+  let index = 0
+  let count = 0
+  while (instructions[index] != null) {
+    const nextIndex = index + instructions[index]
+    instructions[index] = increment(instructions[index])
+    index = nextIndex
+    count++
+  }
+  return count
+}
+
+const part1 = input => {
+  return followInstructions(input, offset => offset + 1)
+}
+
+// Part 2
+// ======
+
+const part2 = input => {
+  return followInstructions(input, offset => {
+    return offset >= 3 ? offset - 1 : offset + 1
+  })
+}
+
+module.exports = { part1, part2 }
