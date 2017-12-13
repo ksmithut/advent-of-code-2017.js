08c0cf3be7e1c84cbfe3be492eb4ccde9d7b5bc0
ksmithut <ksmith.ut@gmail.com>
2017-12-12 23:17:26 -0700

    day13

---
+++ days/day13.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// Part 1
+// ======
+
+const createFirewall = input => {
+  return input.split('\n').reduce((layers, line) => {
+    const [depth, range] = line.split(':').map(Number)
+    layers[depth] = range
+    return layers
+  }, [])
+}
+
+const didCollide = (depth, range, delay = 0) => {
+  return (depth + delay) % ((range - 1) * 2) === 0
+}
+
+const part1 = input => {
+  return createFirewall(input).reduce((severity, range, depth) => {
+    if (didCollide(depth, range)) severity += range * depth
+    return severity
+  }, 0)
+}
+
+// Part 2
+// ======
+
+const part2 = input => {
+  const firewall = createFirewall(input)
+  let delay = 0
+  let searching = true
+  while (searching) {
+    searching = firewall.some((range, depth) => {
+      if (didCollide(depth, range, delay)) {
+        delay++
+        return true
+      }
+    })
+  }
+  return delay
+}
+
+module.exports = { part1, part2 }
