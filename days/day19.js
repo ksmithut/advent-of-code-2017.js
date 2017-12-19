408f5c2477929b8a0d54f6bde0da3a8b34fd47f4
ksmithut <ksmith.ut@gmail.com>
2017-12-19 07:35:08 -0700

    cleaned up day19

--- days/day19.js
+++ days/day19.js
@@ -4,121 +4,85 @@
 // ======
 
 const DIRECTIONS = {
-  UP: {
-    next: ({ x, y, ...rest }) => ({ x: x + 0, y: y - 1, ...rest })
-  },
-  DOWN: {
-    next: ({ x, y, ...rest }) => ({ x: x + 0, y: y + 1, ...rest })
-  },
-  LEFT: {
-    next: ({ x, y, ...rest }) => ({ x: x - 1, y: y + 0, ...rest })
-  },
-  RIGHT: {
-    next: ({ x, y, ...rest }) => ({ x: x + 1, y: y + 0, ...rest })
-  }
+  UP: ({ x, y, ...rest }) => ({ x, y: y - 1, ...rest }),
+  DOWN: ({ x, y, ...rest }) => ({ x, y: y + 1, ...rest }),
+  LEFT: ({ x, y, ...rest }) => ({ x: x - 1, y, ...rest }),
+  RIGHT: ({ x, y, ...rest }) => ({ x: x + 1, y, ...rest })
 }
 
 const findStart = grid => {
-  for (let x = 0; x < grid[0].length; x++) {
+  const lastY = grid.length - 1
+  const lastX = grid[0].length - 1
+  for (let x = 0; x <= lastX; x++) {
     if (grid[0][x] === '|') return { x, y: 0, dir: DIRECTIONS.DOWN }
-    if (grid[grid.length - 1][x] === '|') {
-      return { x, y: grid.length - 1, dir: DIRECTIONS.UP }
-    }
+    if (grid[lastY][x] === '|') return { x, y: lastY, dir: DIRECTIONS.UP }
   }
-  for (let y = 0; y < grid.length; y++) {
+  for (let y = 0; y <= lastY; y++) {
     if (grid[y][0] === '-') return { x: 0, y, dir: DIRECTIONS.RIGHT }
-    if (grid[y][grid[0].length - 1] === '-') {
-      return { x: grid[0].length - 1, y, dir: DIRECTIONS.LEFT }
-    }
+    if (grid[y][lastX] === '-') return { x: lastX, y, dir: DIRECTIONS.LEFT }
   }
   return null
 }
 
 const getChar = (grid, marker) => grid[marker.y][marker.x]
 
-const part1 = input => {
-  // TODO fix input trimming
-  const fs = require('fs')
-  input = fs.readFileSync('days/day19input.txt', 'utf8')
-
+const reduceGridPath = (input, fn, initialValue) => {
   const grid = input.split('\n').map(line => line.split(''))
-  const letters = []
+  let value = initialValue
   let marker = findStart(grid)
-  let found = false
-  while (!found) {
-    for (
-      let char = getChar(grid, marker);
-      char !== '+' && char !== ' ';
-      marker = marker.dir.next(marker), char = getChar(grid, marker)
-    ) {
-      if (/\w/.test(char)) letters.push(char)
+  // Note that we break out later
+  while (true) {
+    // Follow the line until the end
+    for (let char = getChar(grid, marker); char !== '+' && char !== ' ';) {
+      // Note that these could also be in the iteraction portion of the loop
+      // definition, but I found it more readable here
+      marker = marker.dir(marker)
+      char = getChar(grid, marker)
+      value = fn(value, char, marker)
     }
-    if (getChar(grid, marker) === ' ') {
-      found = true
-      continue
-    }
+    // If it's a ' ', then we've reached the end
+    if (getChar(grid, marker) === ' ') break
+    // If it's a '+', then we need to check if we go left or right (relative to
+    // the direction we're actually going)
     switch (marker.dir) {
       case DIRECTIONS.UP:
       case DIRECTIONS.DOWN:
         marker.dir =
-          getChar(grid, DIRECTIONS.LEFT.next(marker)) === '-'
+          getChar(grid, DIRECTIONS.LEFT(marker)) === '-'
             ? DIRECTIONS.LEFT
             : DIRECTIONS.RIGHT
         break
       case DIRECTIONS.LEFT:
       case DIRECTIONS.RIGHT:
         marker.dir =
-          getChar(grid, DIRECTIONS.UP.next(marker)) === '|'
+          getChar(grid, DIRECTIONS.UP(marker)) === '|'
             ? DIRECTIONS.UP
             : DIRECTIONS.DOWN
         break
     }
-    marker = marker.dir.next(marker)
+    // Move one step in that direction
+    marker = marker.dir(marker)
+    value = fn(value, getChar(grid, marker), marker)
   }
-  return letters.join('')
+  return value
+}
+
+const part1 = input => {
+  return reduceGridPath(
+    input,
+    (letters, char) => {
+      if (/\w/.test(char)) return letters + char
+      return letters
+    },
+    ''
+  )
 }
 
 // Part 2
 // ======
 
 const part2 = input => {
-  const grid = input.split('\n').map(line => line.split(''))
-  const letters = []
-  let marker = findStart(grid)
-  let steps = 0
-  let found = false
-  while (!found) {
-    for (
-      let char = getChar(grid, marker);
-      char !== '+' && char !== ' ';
-      steps++, marker = marker.dir.next(marker), char = getChar(grid, marker)
-    ) {
-      if (/\w/.test(char)) letters.push(char)
-    }
-    if (getChar(grid, marker) === ' ') {
-      found = true
-      continue
-    }
-    switch (marker.dir) {
-      case DIRECTIONS.UP:
-      case DIRECTIONS.DOWN:
-        marker.dir =
-          getChar(grid, DIRECTIONS.LEFT.next(marker)) === '-'
-            ? DIRECTIONS.LEFT
-            : DIRECTIONS.RIGHT
-        break
-      case DIRECTIONS.LEFT:
-      case DIRECTIONS.RIGHT:
-        marker.dir =
-          getChar(grid, DIRECTIONS.UP.next(marker)) === '|'
-            ? DIRECTIONS.UP
-            : DIRECTIONS.DOWN
-        break
-    }
-    steps++
-    marker = marker.dir.next(marker)
-  }
-  return steps
+  return reduceGridPath(input, steps => steps + 1, 0)
 }
 
 exports.part1 = part1
